c565edf42d58d4a100b2d5d7e53dbc8f4dde8cbd
39370 <39370@172.22.50.46>
2022-03-08 15:25:15 +0100

    Svolgimento esercizio

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -3,6 +3,7 @@ package it.polito.tdp.parole;
 import it.polito.tdp.parole.model.Parole;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,7 +14,7 @@ import javafx.scene.control.TextField;
 public class FXMLController {
 	
 	Parole elenco ;
-
+	
     @FXML
     private ResourceBundle resources;
 
@@ -27,19 +28,47 @@ public class FXMLController {
     private Button btnInserisci;
 
     @FXML
+    private TextArea txtArea;
+    
+    @FXML
     private TextArea txtResult;
 
     @FXML
     private Button btnReset;
 
     @FXML
+	private Button btnCancella;
+    
+    @FXML
+    void doDelete(ActionEvent event) {
+    	long startTime = System.nanoTime();
+    	String parolaDaCancellare = txtResult.getSelectedText();
+    	elenco.delete(parolaDaCancellare);
+    	elenco.getElenco();
+    	txtResult.setText(elenco.toString());
+    	long elapsedNanos = System.nanoTime() - startTime;
+    	txtArea.setText(Long.toString(elapsedNanos)+" ns");
+    }
+    
+    @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	long startTime = System.nanoTime();
+    	String parola = txtParola.getText();
+    	elenco.addParola(parola);
+    	elenco.getElenco();
+    	txtResult.setText(elenco.toString());
+    	txtParola.clear();
+    	long elapsedNanos = System.nanoTime() - startTime;
+    	txtArea.setText(Long.toString(elapsedNanos)+" ns");
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	long startTime = System.nanoTime();
+    	elenco.reset();
+    	txtResult.clear();
+    	long elapsedNanos = System.nanoTime() - startTime;
+    	txtArea.setText(Long.toString(elapsedNanos)+" ns");
     }
 
     @FXML
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,44 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Parole {
-		
+	
+	private List<String> listaparole;
+	
 	public Parole() {
-		//TODO
+		this.listaparole = new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		this.listaparole.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(listaparole, new ComparatoreOrdineAlfabetico());
+		return listaparole;
 	}
 	
 	public void reset() {
-		// TODO
+		this.listaparole = new ArrayList<String>();
+	}
+	
+	public void delete(String parolaDaCancellare) {
+		List<String> listaparole2 = new ArrayList<String>(listaparole);
+		listaparole.clear();
+		for(String s: listaparole2)
+			if(s.compareTo(parolaDaCancellare)!=0)
+				listaparole.add(s);
+	}
+	
+	@Override
+	public String toString() {
+		String risultato = "";
+		for(String s: listaparole)
+			risultato+= s + "\n";
+		return risultato;
 	}
 
 }
